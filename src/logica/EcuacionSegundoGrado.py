6d025b937599e9298488a1728e0813971dc264b5
Brad Rosales <73218792@continental.edu.pe>
2023-05-31 21:14:23 -0500

    Solucion cuando las raices son reales (verde)

--- src/logica/EcuacionSegundoGrado.py
+++ src/logica/EcuacionSegundoGrado.py
@@ -1,4 +1,10 @@
+import math
 class EcuacionSegundoGrado:
-    def calculoECS(self, a, b, c):
-        return None, None
+    def calculoEGS(self, a, b, c):
+        discriminante = pow(b,2)-4*a*c
+        if discriminante >= 0:
+            raizdiscriminante = math.sqrt(discriminante)
+            X1 = (-b + raizdiscriminante) / (2 * a)
+            X2 = (-b - raizdiscriminante) / (2 * a)
+        return X1, X2
 
--- tests/PruebaEcuacionSegundoGrado.py
+++ tests/PruebaEcuacionSegundoGrado.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 from src.logica.EcuacionSegundoGrado import EcuacionSegundoGrado
 
@@ -17,6 +18,28 @@ class PruebaEcuacionSegundoGrado(unittest.TestCase):
         X1Actual, X2Actual = ecuacion.calculoECS(a, b, c)
 
         # Assert
-        self.assertEqual(X1Actual, X1Esperado)
-        self.assertEqual(X2Actual, X2Esperado) # add assertion here
+        precision = math.fabs(X1Esperado - X1Actual)
+        self.assertLessEqual(precision, 0.001)
+        precision = math.fabs(X2Esperado - X2Actual)
+        self.assertLessEqual(precision, 0.001)
 
+
+    def test_EcuacionSegundoGrado_parametrosNumericosMultiple_RaicesReales(self):
+        ecuacion = EcuacionSegundoGrado()
+        items = (
+            {"Case": "Caso 01", "a": 3, "b": -5, "c": 1, "RaizEsperada1": 1.43, "RaizEsperada2": 0.23},
+            {"Case": "Caso 02", "a": 1, "b": 2, "c": 1, "RaizEsperada1": -1.00, "RaizEsperada2": -1.00},
+            {"Case": "Caso 03", "a": -1, "b": 2, "c": -1, "RaizEsperada1": 1.00, "RaizEsperada2": 1.00},
+            {"Case": "Caso 04", "a": 1, "b": 0, "c": -18, "RaizEsperada1": 4.24, "RaizEsperada2": -4.24},
+            {"Case": "Caso 05", "a": 1, "b": 4, "c": 0, "RaizEsperada1": 0.00, "RaizEsperada2": -4.00},
+            {"Case": "Caso 06", "a": 1, "b": 4, "c": 4, "RaizEsperada1": -2.00, "RaizEsperada2": -2.00},
+            {"Case": "Caso 07", "a": 1, "b": 3, "c": 2, "RaizEsperada1": -1.00, "RaizEsperada2": -2.00},
+        )
+        # do
+        for item in items:
+            with self.subTest(item["Case"]):
+                RaizActual1, RaizActual2 = ecuacion.calculoEGS(item["a"], item["b"], item["c"])
+
+        #assert
+        self.assertEqual(item["RaizEsperada1"], RaizActual1)
+        self.assertEqual(item["RaizEsperada2"], RaizActual2)
